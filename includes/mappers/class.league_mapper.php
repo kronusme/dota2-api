91c8e05b66bbfceb2963789b86f20b6cf1fc6472
kronusme <kronusme@gmail.com>
2013-02-21 00:09:27 +0200

    Live matches mapper improve

--- includes/mappers/class.league_mapper.php
+++ includes/mappers/class.league_mapper.php
@@ -72,17 +72,18 @@ class league_mapper {
             }
             $a_game = (array)$game;
             unset($a_game['players']);
-            $a_game['radiant_team_id'] = $a_game['radiant_team']['team_id'];
-            $a_game['radiant_name'] = $a_game['radiant_team']['team_name'];
-            $a_game['radiant_logo'] = $a_game['radiant_team']['team_logo'];
-            $a_game['radiant_team_complete'] = ($a_game['radiant_team']['complete'] == 'false')? 0 : 1;
-            $a_game['dire_team_id'] = $a_game['dire_team']['team_id'];
-            $a_game['dire_name'] = $a_game['dire_team']['team_name'];
-            $a_game['dire_logo'] = $a_game['dire_team']['team_logo'];
-            $a_game['dire_team_complete'] = ($a_game['dire_team']['complete'] == 'false')? 0 : 1;
+            print_r($a_game);
+            $a_game['radiant_team_id'] = (string)$a_game['radiant_team']->team_id;
+            $a_game['radiant_name'] = (string)$a_game['radiant_team']->team_name;
+            $a_game['radiant_logo'] = (string)$a_game['radiant_team']->team_logo;
+            $a_game['radiant_team_complete'] = ($a_game['radiant_team']->complete == 'false')? 0 : 1;
+            $a_game['dire_team_id'] = (string)$a_game['dire_team']->team_id;
+            $a_game['dire_name'] = (string)$a_game['dire_team']->team_name;
+            $a_game['dire_logo'] = (string)$a_game['dire_team']->team_logo;
+            $a_game['dire_team_complete'] = ($a_game['dire_team']->complete == 'false')? 0 : 1;
             unset($a_game['dire_team']);
             unset($a_game['radiant_team']);
-            //$live_match->set_array($a_game);
+            $live_match->set_array($a_game);
             array_push($live_matches, $live_match);
         }
         return $live_matches;
