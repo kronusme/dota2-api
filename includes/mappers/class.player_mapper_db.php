34e56d1ca0d23ebfe17260d521c752a000dc149c
Eric Pfeiffer <computer.fr33k@gmail.com>
2013-06-23 21:30:24 -0500

    updated player mapper db class

--- includes/mappers/class.player_mapper_db.php
+++ includes/mappers/class.player_mapper_db.php
@@ -14,6 +14,10 @@ class player_mapper_db extends player_db {
 		return $this->_steam_id;
 	}
 	
+	/**
+	 * @param steam_id or null
+	 * @return player_db object
+	 */
 	public function load($id = null) {
 		if(!is_null($id)) {
 			$this->_steam_id = (string)$id;
@@ -30,13 +34,37 @@ class player_mapper_db extends player_db {
 		return $player;
 	}
 	
-	public function save() {
+	public function save(player_db $player) {
+		if(player_mapper_db::player_exists($player->get('steam_id'))) {
+			$this->update($player);
+		}
+		else {
+			$this->insert($player);
+		}
 	}
 	
-	private function insert() {
+	private function insert($player) {
+		$db = db::obtain();
+		$data = array(
+			'personaname' => $player->get('personaname'),
+			'steam_id' => $player->get('steam_id'),
+			'community_visibility_state' => $player->get('communityvisibilitystate'),
+			'profile_state' => $player->get('profilestate'),
+			'last_logoff' => $player->get('lastlogoff'),
+			'comment_permission' => $player->get('commentpermission'),
+			'profile_url' => $player->get('profileurl'),
+			'avatar' => $player->get('avatar'),
+			'avatar_medium' => $player->get('avatarmedium'),
+			'avatar_full' => $player->get('avatarfull'),
+			'personastate' => $player->get('personastate'),
+			'real_name' => $player->get('realname'),
+			'primary_clan_id' => $player->get('primaryclanid'),
+			'time_created' => $player->get('timecreated')
+			);
+		$db->insert_pdo(db::real_tablename('users'), $data);
 	}
 	
-	public function update($player) {
+	private function update($player) {
 		$db = db::obtain();
 		$data = array(
 			'personaname' => $player->get('personaname'),
@@ -62,11 +90,12 @@ class player_mapper_db extends player_db {
 	 * @return bool
 	 */
 	public static function player_exists($id = null) {
-		if(!is_null($id)) {
-			$this->set_steamid($id);
+		if(is_null($id)) {
+			return;
 		}
+		
 		$db = db::obtain();
-		$result = $db->query_first_pdo('SELECT * FROM ' . db::real_tablename('users') . ' WHERE steam_id = ?', array($this->get_steamid()));
+		$result = $db->query_first_pdo('SELECT * FROM ' . db::real_tablename('users') . ' WHERE steam_id = ?', array($id));
 		if($result['steam_id'] == $this->get_steamid) {
 			return true;
 		}
