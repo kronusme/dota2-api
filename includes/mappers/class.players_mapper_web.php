46e06ec4765af8c5c9dec2dafe4d3429cd981fac
Luchnik22 <oleg@loutchansky.com>
2014-06-15 15:48:45 +0400

    Add ResolveVanityURL players mapper web

--- includes/mappers/class.players_mapper_web.php
+++ includes/mappers/class.players_mapper_web.php
@@ -5,7 +5,7 @@
  * @example
  * <code>
  *  $players_mapper_web = new players_mapper_web();
- *  $players_info = $players_mapper_web->add_id('76561198067833250')->add_id('76561198058587506')->load();
+ *  $players_info = $players_mapper_web->add_id('76561198067833250')->add_nickname('Nimf0manka')->load();
  *  foreach($players_info as $player_info) {
  *    echo $player_info->get('realname');
  *    echo '<img src="'.$player_info->get('avatarfull').'" alt="'.$player_info->get('personaname').'" />';
@@ -20,9 +20,17 @@ class players_mapper_web {
      */
     const player_steam_url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/';
     /**
+     * 
+     */
+    const resolve_steam_url = 'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/';
+    /**
      * @var array
      */
     private $_ids = array();
+    /**
+     * @var array
+     */
+    private $_nicknames = array();
 
     /**
      * @param $id
@@ -73,6 +81,52 @@ class players_mapper_web {
     }
 
     /**
+     * @param $nickname
+     * @return players_mapper_web
+     */
+    public function add_nickname($nickname)
+    {
+        $nickname = (string)$nickname;
+        if (!in_array($nickname, $this->_nicknames)) {
+            array_push($this->_nicknames, $nickname);
+        }
+        return $this;
+    }
+
+    public function remove_nickname($nickname)
+    {
+        $nickname = (string)$nickname;
+        foreach($this->_nicknames as $k => $v) {
+            if ($v == $nickname) {
+                unset($this->_ids[$k]);
+            }
+        }
+        return $this;
+    }
+
+    /**
+    * @return players_mapper_web
+    */
+    public function remove_nicknames() {
+        $this->_nicknames = array();
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function get_nicknames() {
+        return $this->_nicknames;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_nicknames_string() {
+        return implode(',', $this->get_nicknames());
+    }
+
+    /**
      *
      */
     public function __construct() {
@@ -83,7 +137,20 @@ class players_mapper_web {
      * @return array
      */
     public function load() {
+        foreach ($this->_nicknames as $k => $v) {
+            $request = new request(self::resolve_steam_url, array('vanityurl' => $v));
+            $nickname_info = $request->send();
+            if (is_null($nickname_info)) {
+                continue;
+            }
+            $id = $nickname_info->steamid;
+            if (!in_array($id, $this->_ids)) {
+                array_push($this->_ids, $id);
+            }
+        }
+
         $request = new request(self::player_steam_url, array('steamids' => $this->get_ids_string()));
+
         $players_info = $request->send();
         if (is_null($players_info)) {
             return null;
