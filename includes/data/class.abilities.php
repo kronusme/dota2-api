fcbfa87c543045e4f733e03287ae4a92cc144c0e
kronusme <kronusme@gmail.com>
2014-11-27 17:48:41 +0200

    More and more tests added

--- includes/data/class.abilities.php
+++ includes/data/class.abilities.php
@@ -8,7 +8,7 @@
  * <code>
  *   $abilities = new abilities();
  *   $abilities->parse();
- *   $abilities-get_data_by_id(5172); // return array for ability with id 5172 (BeastMaster Inner Beast)
+ *   $abilities->get_data_by_id(5172); // return array for ability with id 5172 (BeastMaster Inner Beast)
  *   // same, because there are no thumbs for abilities
  *   $abilities->get_img_url_by_id(5172, false);
  *   $abilities->get_img_url_by_id(5172);
--- tests/includes/mappers/class.league_prize_pool_mapper_webTest.php
+++ tests/includes/mappers/class.league_prize_pool_mapper_webTest.php
@@ -6,7 +6,7 @@ class league_prize_pool_mapper_webTest extends PHPUnit_Framework_TestCase
         $league_prize_pool_mapper_web = new league_prize_pool_mapper_web();
         $league_prize_pool_mapper_web->set_league_id(600);
         $prize_pool_info = $league_prize_pool_mapper_web->load();
-        $this->assertTrue(intval($prize_pool_info['prize_pool']) > 0);
+        $this->assertTrue(intval($prize_pool_info['prize_pool']) >= 0);
         $this->assertEquals(strval($prize_pool_info['league_id']), '600');
     }
 }
