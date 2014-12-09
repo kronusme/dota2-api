d3b41dda126f83882c643003c13d99fbace31151
kronusme <kronusme@gmail.com>
2014-12-09 21:05:10 +0200

    #73 Rename all files to use PSR-4 standard

--- includes/Api.php
+++ includes/Api.php
@@ -8,7 +8,7 @@ use Dota2Api\Utils\Request;
 class Api {
 
     public static function init($api_key, array $db, $autoselect_db = true) {
-        $db = call_user_func_array('Db::obtain', $db);
+        $db = call_user_func_array('Dota2Api\Utils\Db::obtain', $db);
         $db->connect_pdo($autoselect_db);
         Request::$api_key = strval($api_key);
     }
