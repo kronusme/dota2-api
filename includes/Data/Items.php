9eee0404baeeae80450da64ed48d00327e948ccf
kronusme <kronusme@gmail.com>
2015-05-11 12:39:42 +0300

    Support Frosty-items mapping

--- includes/Data/Items.php
+++ includes/Data/Items.php
@@ -22,9 +22,48 @@ class Items extends HeroesData
      */
     const EMPTY_ID = 0;
 
+    /**
+     * Some items where replaced in 6.84-patch
+     * Example:
+     * before 6.84 - mystery_hook was 217
+     * 6.84+ - mystery_hook is 1001
+     * @var array (old item id => new item id)
+     */
+    protected $_conflictsMap = array(
+        217 => 1001,
+        218 => 1002,
+        219 => 1003,
+        220 => 1004,
+        221 => 1005,
+        226 => 1006,
+        228 => 1007,
+        235 => 1008,
+        227 => 1009,
+        229 => 1010,
+        230 => 1011,
+        231 => 1012,
+        232 => 1013,
+        233 => 1014,
+        234 => 1015,
+        236 => 1016,
+        237 => 1017,
+        238 => 1018,
+        239 => 1019,
+        240 => 1020
+    );
+
     public function __construct()
     {
         $this->setFilename('items.json');
         $this->setField('items');
+    }
+
+    public function getImgUrlById($id, $thumb = true, $isConflict = false)
+    {
+        $id = (int)$id;
+        if ($isConflict) {
+            $id = array_key_exists($id, $this->_conflictsMap) ? $this->_conflictsMap[$id] : $id;
+        }
+        return parent::getImgUrlById($id, $thumb);
     }
 }
---
+++ tests/includes/Data/ItemsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Dota2Api\Data\Items;
+
+class AbilitiesTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Items
+     */
+    public $items;
+
+    public function setUp()
+    {
+        $this->items = new Items();
+        $this->items->parse();
+    }
+
+    public function testGetImgUrlById()
+    {
+        $url = $this->items->getImgUrlById(220, true, true);
+        $this->assertContains('mystery_toss', $url);
+
+        $url = $this->items->getImgUrlById(220, true, false);
+        $this->assertContains('travel_boots_2', $url);
+    }
+}
