e8085c3525352a0c7cd8dedcfb18edfb28424891
kronusme <kronusme@gmail.com>
2016-07-07 10:43:19 +0300

    Add TeamsMapperDb

---
+++ includes/Mappers/TeamsMapperDb.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Dota2Api\Mappers;
+
+use Dota2Api\Models\Team;
+use Dota2Api\Utils\Db;
+
+/**
+ * Load info about team from web
+ *
+ * @author kronus
+ * @example
+ * <code>
+ *   $teamsMapperDb = new Dota2Api\Mappers\TeamsMapperDb();
+ *   $teams = $teamsMapperDb->load();
+ *   foreach($teams as $team) {
+ *     echo $team->get('name');
+ *   }
+ * </code>
+ */
+class TeamsMapperDb extends TeamsMapper
+{
+    /**
+     *
+     */
+    const TEAMS_STEAM_URL = 'https://api.steampowered.com/IDOTA2Match_570/GetTeamInfoByTeamID/v001/';
+
+    /**
+     * @return null|Team[]
+     */
+    public function load($tid)
+    {
+        $addSql = '';
+        if (!is_null($tid)) {
+            if (is_array($tid)) {
+                $addSql = ' WHERE id IN (' . implode(',', $tid) . ')';
+            } else {
+                $addSql = ' WHERE id = ' . intval($tid);
+            }
+        }
+        $db = Db::obtain();
+        $result = $db->fetchArrayPDO('SELECT * FROM ' . Db::realTablename('teams') . ' ' . $addSql);
+        if ($result === false) {
+            return null;
+        }
+        $teams = array();
+        foreach ($result as $row) {
+            $tid = $row['id'];
+            $team = new Team();
+            $team->setArray($row);
+            $teams[$tid] = $team;
+        }
+        return $teams;
+    }
+
+    /**
+     * @param Team $team
+     * @return bool|integer
+     */
+    public function save($team)
+    {
+        $db = Db::obtain();
+        return $db->insertPDO(Db::realTablename('teams'),
+            array('id' => $team->get('id'), 'name' => $team->get('name')));
+    }
+
+    /**
+     * @param integer[] $ids
+     */
+    public function delete($ids)
+    {
+        $db = Db::obtain();
+        $ids = is_array($ids) ? $ids : array($ids);
+        $db->exec('DELETE FROM ' . Db::realTablename('teams') . ' WHERE id IN (' . implode(',', $ids) . ')');
+    }
+}
---
+++ tests/includes/Mappers/TeamsMapperDbTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Dota2Api\Mappers\TeamsMapperWeb;
+use Dota2Api\Mappers\TeamsMapperDb;
+
+class TeamsMapperDbTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var TeamsMapperWeb
+     */
+    protected $mapper;
+
+    protected function setUp()
+    {
+        $this->mapper = new TeamsMapperDb();
+        $mapperWeb = new TeamsMapperWeb();
+        $mapperWeb->setTeamId(36)->setTeamsRequested(1);
+        $teams = $mapperWeb->load();
+
+        $this->assertEquals(count($teams), 1);
+        $team = array_pop($teams);
+        $team->set('id', 36);
+        $this->mapper->save($team);
+    }
+
+    public function testLoad()
+    {
+        $teams = $this->mapper->load(36);
+        $this->assertEquals(count($teams), 1);
+        $team = $teams[36];
+        $this->assertEquals($team->get('name'), 'Natus Vincere');
+    }
+
+}
